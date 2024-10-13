f0fd6c8c7b2c71a5feb649ac44034ca4051895fa
Xiaoshan Lin <lin00668@umn.edu>
2024-10-13 00:18:00 -0400

    Modify to accommodate changes in python package; Organize code

--- test_coordinator/test_coordinator/robot_ros.py
+++ test_coordinator/test_coordinator/robot_ros.py
@@ -1,7 +1,6 @@
 import rclpy
 from rclpy.node import Node
 from test_interfaces.msg import TaskAllocation
-from std_msgs.msg import String
 from test_interfaces.srv import RequestAllocation, SendData  # Assuming RobotTask service exists
 import numpy as np
 from MRTAALwTL.robot import Robot
@@ -9,8 +8,30 @@ import yaml
 
 
 class RobotROS(Node):
-    def __init__(self, robot, robot_id):
-        super().__init__('robot_' + str(robot_id))
+    def __init__(self):
+        super().__init__('robot',
+                         allow_undeclared_parameters=True,
+                         automatically_declare_parameters_from_overrides=True,
+                         )
+
+        config_path = self.get_parameter('config_path').value
+        robot_config_path = self.get_parameter('robot_config_path').value
+        robot_id = self.get_parameter('robot_id').value
+        robot_type = self.get_parameter('robot_type').value
+        robot_class = self.get_parameter('robot_class').value
+        proj_dir = self.get_parameter('proj_dir').value
+        base_seed = self.get_parameter('base_seed').value
+
+        # Load YAML configurations
+        with open(config_path, 'r') as f:
+            config = yaml.safe_load(f)
+
+        with open(robot_config_path, 'r') as f:
+            robot_config = yaml.safe_load(f)
+
+        # Create the Robot instance (learning and estimation logic)
+        robot = Robot(robot_id, robot_class, robot_type, config, robot_config, proj_dir, base_seed)
+
         self.robot = robot  # Use the Robot instance
         self.num_robots = self.robot.num_robots
         self.num_tasks = self.robot.num_tasks
@@ -24,11 +45,11 @@ class RobotROS(Node):
             self.task_allocation_callback,
             10)
 
-        self.string_subscription = self.create_subscription(
-            String,
-            'proj_dir',
-            self.pickle_callback,
-            10)
+        # self.string_subscription = self.create_subscription(
+        #     String,
+        #     'proj_dir',
+        #     self.pickle_callback,
+        #     10)
 
         # Wait for the coordinator’s service to be available
         while not self.client.wait_for_service(timeout_sec=1.0):
@@ -41,13 +62,15 @@ class RobotROS(Node):
 
         self.send_values()
 
-    def pickle_callback(self, msg):
-        proj_dir = msg.data
-        self.robot.pickle(proj_dir)
-        self.get_logger().info(f'Robot {self.robot.robot_id} received project dir')
+    # def pickle_callback(self, msg):
+    #     proj_dir = msg.data
+    #     self.get_logger().info(f'Robot {self.robot.robot_id} received project dir')
 
     def send_values(self):
         values, probs, backup_probs = self.robot.get_values_and_probs()
+        values = np.array(values, dtype=np.float32).tolist()
+        probs = np.array(probs, dtype=np.float32).tolist()
+        backup_probs = np.array(backup_probs, dtype=np.float32).tolist()
 
         # self.get_logger().info(f'mdp:{self.robot.current_mdp_state}')
         # self.get_logger().info(f'lb: {self.robot.estimator.lower_bound[0][self.robot.current_mdp_state]}')
@@ -70,14 +93,10 @@ class RobotROS(Node):
         if feasible:
             task_allocation = data.reshape(self.num_robots, self.num_tasks + 1)
 
-            probs = np.array(task_allocation[self.robot.robot_id])
-            probs = probs / probs.sum()  # normalized
-            task = np.random.choice(self.num_tasks + 1, p=probs)
-
             # Process the task allocation with the Robot class
-            status = self.robot.execute(task)
+            status = self.robot.execute(task_allocation)
 
-            # self.get_logger().info(f'mdp: {self.robot.current_mdp_state}')
+            # self.get_logger().info(f'robot {self.robot.robot_id}, result: {action_result}')
             # self.get_logger().info(f'lb: {self.robot.estimator.lower_bound[0][self.robot.current_mdp_state]}')
 
             if status == 'Continue':
@@ -85,12 +104,14 @@ class RobotROS(Node):
 
             else:
                 self.send_results()
-                if status == 'Episode end':
+                if status == 'Iteration end':
                     self.get_logger().info(f'Reached the desired learning episode for iteration {self.robot.repeat_count}')
+                    self.robot.pickle()
                     self.reset()
                     self.send_values()
                 else:
                     self.get_logger().info(f'Reached the desired learning episode for {self.robot.repeat_iters} iterations')
+                    self.robot.pickle()
         else:
             raise RuntimeError("Unalbe to find feasible task allocation.")
 
@@ -113,38 +134,14 @@ class RobotROS(Node):
 def main(args=None):
     rclpy.init(args=args)
 
-    # Initialize a temporary ROS2 node to handle parameter retrieval
-    robot_node = Node('temp_node_to_get_parameters')
-
-    # Get the parameters for the configuration file paths, robot_id, and robot_type
-    config_path = robot_node.declare_parameter('config_path', '').value
-    robot_config_path = robot_node.declare_parameter('robot_config_path', '').value
-    robot_id = robot_node.declare_parameter('robot_id', 0).value
-    robot_type = robot_node.declare_parameter('robot_type', 'type_1').value
-
-    # Load YAML configurations
-    with open(config_path, 'r') as f:
-        config = yaml.safe_load(f)
-
-    with open(robot_config_path, 'r') as f:
-        robot_config = yaml.safe_load(f)
-
-    # Create the Robot instance (learning and estimation logic)
-    robot = Robot(robot_id, robot_type, config=config, robot_config=robot_config)
-
-    # Destroy the temporary node after parameters are fetched
-    robot_node.destroy_node()
-
     try:
         # Now create the actual ROS interface using the Robot instance
-        robot_ros = RobotROS(robot, robot_id)
-
-        # Spin the node (this is the actual ROS2 node handling the logic)
-        rclpy.spin(robot_ros)
+        robot = RobotROS()
+        rclpy.spin(robot)
     except Exception as exception:
         raise exception
     else:
-        robot_ros.destroy_node()
+        robot.destroy_node()
         rclpy.shutdown()
 
 
